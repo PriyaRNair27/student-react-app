535d659b7111d3abae606753876fcd3d8980228c
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-16 20:12:11 +0530

    search

---
+++ src/Components/Searchstudent.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Searchstudent = () => {
+  return (
+    <div>
+<div classNameName="container">
+    <div classNameName="row">
+        <div classNameName="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div classNameName="container">
+                <div classNameName="row g-3">
+                   <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                        <label for="" className="for">university number</label>
+                        <input placeholder="enter university number" type="text" className="form-cotrol"/>
+                    </div>
+                    
+                    <div classNameName="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <button className="btn btn-danger">search</button>
+                    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Searchstudent
