13c7880fee3adb58368127b9b3600e5aab159032
PriyaRNair27 <priyarnair2710@gmail.com>
2022-05-17 11:25:10 +0530

    navbaradded

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Addstudent from './Components/Addstudent';
 import Search from './Components/Search';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-       <Addstudent/>
-      <Search/>
-     
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={ <Addstudent/> }/>
+      <Route path="/search" exact element={ <Search/> }/>
+    </Routes>
+    </BrowserRouter>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,22 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <div>
 
-<nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-dark bg-success">
+  <div className="container-fluid">
+    <Link to={"/"} className="navbar-brand">MZC</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Add student</a>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+         <Link to={"/"} className="nav-link active">Addstudent</Link>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Search student</a>
+        <li className="nav-item">
+         <Link to={"/search"} className="nav-link">Search Student</Link>
         </li>
         
       </ul>
